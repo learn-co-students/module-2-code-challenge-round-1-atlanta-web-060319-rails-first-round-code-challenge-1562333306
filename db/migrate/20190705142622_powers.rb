0d5dfe07866a8ba835fffd23f3f8f90537a556a3
Brad Bartlett <bradbartlett1@me.com>
2019-07-05 10:57:54 -0400

    message

---
+++ db/migrate/20190705142622_powers.rb
@@ -0,0 +1,11 @@
+class Powers < ActiveRecord::Migration[5.1]
+  def change
+    
+        create_table :powers do |t|
+          t.string :name
+          t.string :description
+    
+          t.timestamps
+        end
+      end
+end
