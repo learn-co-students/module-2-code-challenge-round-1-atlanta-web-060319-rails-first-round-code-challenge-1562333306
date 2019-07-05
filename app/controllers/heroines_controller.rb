a73ad5ff44d24233c19ea823cf1d3eaf9673df83
github username <github email address>
2019-07-05 10:54:33 -0400

    got the search bar to work but couldnt figure out to to make it account for captalization and white spaces

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,7 +1,10 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
   end
+
+  
+
   def show
     @heroine = Heroine.find(params[:id])
 end
@@ -23,7 +26,7 @@ end
 private
 
 def heroine_params
-    params.require(:heroine).permit(:name, :super_name, :power_id)
+    params.require(:heroine).permit(:name, :super_name, :power_id, :search)
  end
 
 end
