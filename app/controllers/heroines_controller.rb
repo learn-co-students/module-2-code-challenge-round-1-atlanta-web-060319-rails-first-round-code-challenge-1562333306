ddcce4c5cbb2a28d8a4b49b941a106b4810b6cc1
MaryJay787 <josephmaryam.mj@gmail.com>
2019-07-05 10:56:55 -0400

    Module 2 Code Challenge Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    if params[:search]
+      @heroines.each {|her| @power = her.power.name = Power.search(params[:search]).order("created_at DESC")}
+      render :search
+    else
+      @heroines.each {|hers| hers.power.name = Power.all.order('created_at DESC') }
+    end
+  end
+
+  def show
+    @her = Heroine.find(params[:id])
+  end
+
+  def new
+    @her = Heroine.new
+  end
+
+  def create
+    @her = Heroine.new(heroine_params)
+    if @her.save
+      redirect_to heroine_path(@her)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,8 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @po = Power.find(params[:id])
+  end
 end
